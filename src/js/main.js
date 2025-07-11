8ddcd2ff55ccb560d39e8145921f142a81f87891
Guilherme Breve <foxtec198@gmail.com>
2025-07-11 07:44:28 -0300

    .

--- src/js/main.js
+++ src/js/main.js
@@ -16,7 +16,7 @@ async function get_analytics(client = '0'){
         req = await fetch(
             server + `?unit_id=${unit_id}&&client_id=${client}`
         )
-    }else{
+    }else{  
         req = await fetch(server + `?unit_id=${unit_id}`)
     }
     const res = await req.json()
@@ -25,11 +25,59 @@ async function get_analytics(client = '0'){
         "CLICK": 0,
         "FORM": 0
     }
+    const listViews = document.getElementById('listViews')
+    listViews.innerHTML = ''
+
     if(req.ok){
+        cont = 0
         res.forEach(item => {
             types[item['type']] += 1
+            if(item.dash){dash(item.dash)}
+            if(!item.dash){
+                const data = new Date(item.data)
+                const opcoes = {
+                    day: '2-digit',
+                    month: 'short',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    hour12: false
+                };
+
+                const li = document.createElement('li')
+                li.classList.add('list-group-item', 'list-group-item-action')
+
+                const dv = document.createElement('div')
+                dv.classList.add('d-flex','flex-column')
+
+                sp1 = document.createElement('span')
+                sp1.innerHTML = '<i class="bi bi-building-fill"></i> Cliente: ' + item.name
+
+                sp2 = document.createElement('span')
+                sp2.innerHTML = `<i class="bi bi-menu-up"></i> Tipo: ${item.type} IP: ${item.ip}`
+
+                sp3 = document.createElement('span')
+                sp3.innerHTML = `<i class="bi bi-calendar-check-fill"></i> ${data.toLocaleDateString('pt-BR', opcoes).replace('.','')}`  
+                
+                dv.appendChild(sp1)
+                dv.appendChild(sp2)
+                dv.appendChild(sp3)
+
+                li.appendChild(dv)
+                listViews.appendChild(li)
+                cont += 1
+            }
         });
-        
+        if(cont === 0){
+            sp = document.createElement('span')
+            sp.textContent = 'Sem dados ainda!'
+
+            listViews.classList.add('d-flex', 'justify-content-center', 'align-items-center', 'text-center', 'justify-self-end')
+            listViews.appendChild(sp)
+            listViews.style.height = '350px'
+        }else{
+            listViews.classList.remove('d-flex', 'justify-content-center', 'align-items-center', 'text-center', 'justify-self-end')
+        }
+
         document.getElementById('views').textContent = types.VIEW
         document.getElementById('clicks').textContent = types.CLICK
         document.getElementById('forms').textContent = types.FORM
@@ -63,7 +111,7 @@ async function get_clients(){
     }
 } 
 
-function dash(){
+function dash(data = {'dias':0, 'views':0, 'forms':0, 'clicks':0}){
         // Dashboard Vendas
         var dv = document.getElementById('graph')
         var dashVendas = document.createElement('canvas')
@@ -72,32 +120,32 @@ function dash(){
         new Chart(dashVendas, {
             type: 'line',
             data: {
-            labels: [1,2,3,4], // DIAS
+            labels: data['dias'], // DIAS
             
             datasets: [{
-                data: [10,20,30,60],
+                data: data['views'],
                 label: 'Visualizações',
                 fill: {
                     target: 'origin',
                 },
                 borderWidth: 1,
-                borderColor: '#fff',
+                borderColor: '#ae2012',
             },{
-                data: [1,10,5,3],
+                data: data['clicks'],
                 label: 'Cliques',
                 fill: {
                     target: 'start',
                 },
                 borderWidth: 2,
-                borderColor: '#f0f0f0',
+                borderColor: '#588157',
             },{
-                data: [1,11,20,35],
+                data: data['forms'],
                 label: 'Formularios',
                 fill: {
                     target: 'start',
                 },
                 borderWidth: 2,
-                borderColor: '#f0f0f0',
+                borderColor: '#0077b6',
             }]
             },
             options: {
@@ -113,10 +161,10 @@ function dash(){
                 }
             },
             plugins: {
-                // title: {
-                //   display: true,
-                //   text: 'PRODUTOS POR ' + opt.toUpperCase()
-                // }
+                title: {
+                  display: true,
+                  text: 'Metricas por dia!'
+                }
             },
             }
         })
